96aad1343b31cbf9c0d94079ee28d159a0a2be62
mrjrwedge2 <corey.p.student@gmail.com>
2018-07-27 22:15:09 -0500

    Added following pages: ContactPage, ContactListPage, EditContactPage, AddContactPage, SearchContactPage, SettingsPage. All pages are as initialized, nothing added for functionality, except for app.module.ts.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,23 +3,44 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { IonicStorageModule} from "@ionic/storage";
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import {ContactPage} from "../pages/contact/contact";
+import {AddContactPage} from "../pages/add-contact/add-contact";
+import {ContactListPage} from "../pages/contact-list/contact-list";
+import {EditContactPage} from "../pages/edit-contact/edit-contact";
+import {SearchContactPage} from "../pages/search-contact/search-contact";
+import {SettingsPage} from "../pages/settings/settings";
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    ContactPage,
+    AddContactPage,
+    ContactListPage,
+    EditContactPage,
+    SearchContactPage,
+    SettingsPage
+
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    IonicStorageModule.forRoot()
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    ContactPage,
+    AddContactPage,
+    ContactListPage,
+    EditContactPage,
+    SearchContactPage,
+    SettingsPage
   ],
   providers: [
     StatusBar,
